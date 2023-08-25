0d330b14519772553e8faecd4a6b1bbdcb3a1cca
KuksovSV <ser.kuksov@mail.ru>
2023-08-25 11:54:09 +0300

    Путь к БД для миграция

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -5,11 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from setings import SQL_URL
 from batadase import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', SQL_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
