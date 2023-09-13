aab15d0d49a85f8bd6ff5528b5c9d773843d61b4
KuksovSV <ser.kuksov@mail.ru>
2023-09-13 20:56:22 +0300

    Тест mypy

--- tests/test_auth/conftest.py
+++ tests/test_auth/conftest.py
@@ -5,6 +5,8 @@ from sqlalchemy import select
 from auth.models import User
 from tests.conftest import async_session_maker
 
+print(1 + 1)
+
 
 async def get_user_from_database(user_id: int) -> User:
     async with async_session_maker() as session:
