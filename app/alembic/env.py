7a9f360f8f77fa6da688062d8e862f0988ef9ce2
KuksovSV <ser.kuksov@mail.ru>
2023-09-13 21:07:54 +0300

    Исправлено удаление импортов + настройка ruff

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -7,11 +7,13 @@ from alembic import context
 
 from setings import settings
 from db import Base
+import note_app.models
+import auth.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', settings.SQL_URL)
+config.set_main_option("sqlalchemy.url", settings.SQL_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -68,9 +70,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
