63b7d408960c410d8d775ed845630418ef05ffac
KuksovSV <ser.kuksov@mail.ru>
2023-09-17 10:04:03 +0300

    Переделаны все роутеры заметок

--- app/utils/repository.py
+++ app/utils/repository.py
@@ -1,13 +1,15 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
-from sqlalchemy import insert, select
+from fastapi_filter.contrib.sqlalchemy import Filter
+from sqlalchemy import insert, select, update, delete, Select
 
 from db import async_session_maker, Base
 
 
 class AbstractRepository(ABC):
     @abstractmethod
-    async def add_one(self, data: dict):
+    async def add_one(self, **kwargs):
         raise NotImplementedError
 
     @abstractmethod
@@ -18,16 +20,66 @@ class AbstractRepository(ABC):
 class SQLAlchemyRepository(AbstractRepository):
     model = Base
 
-    async def add_one(self, data: dict) -> int:
+    async def add_one(self, **kwargs) -> Any:
         async with async_session_maker() as session:
-            stmt = insert(self.model).values(**data).returning(self.model.id)
+            stmt = insert(self.model).values(**kwargs).returning(self.model)
             res = await session.execute(stmt)
             await session.commit()
-            return res.scalar_one()
+            return res.scalar()
 
-    async def find_one(self, elm_id: int):
+    async def find_one(self, elm_id: int) -> Any:
         async with async_session_maker() as session:
-            stmt = select(self.model).where(self.model.id == elm_id)
-            res = await session.execute(stmt)
+            query = select(self.model).where(self.model.id == elm_id)
+            res = await session.execute(query)
             res = res.scalar()
             return res
+
+    async def update_elm(self, elm_id: int, **kwargs) -> bool:
+        async with async_session_maker() as session:
+            stmt = update(self.model).where(self.model.id == elm_id)
+            if kwargs is not None:
+                stmt = stmt.values(**kwargs)
+            res = await session.execute(stmt)
+            await session.commit()
+            return res.rowcount > 0
+
+    async def delete_elm(self, elm_id: int) -> bool:
+        async with async_session_maker() as session:
+            stmt = delete(self.model).where(self.model.id == elm_id)
+            res = await session.execute(stmt)
+            await session.commit()
+            return res.rowcount > 0
+
+    @staticmethod
+    async def _find_elements(
+        query: Select,
+        filter_elm: Filter,
+        limit: int,
+        page: int,
+    ) -> list[Any]:
+        skip = (page - 1) * limit
+        async with async_session_maker() as session:
+            # query = self.get_query()
+            if filter_elm is not None:
+                query = filter_elm.filter(query)
+                if hasattr(filter_elm, "order_by"):
+                    query = filter_elm.sort(query)
+            query = query.limit(limit).offset(skip)
+            res = await session.execute(query)
+            return res.scalars().all()
+
+    def get_query(self) -> Select:
+        return select(self.model)
+
+    async def find_elements(
+        self,
+        filter_elm: Filter = None,
+        limit: int = 25,
+        page: int = 1,
+    ) -> list[Any]:
+        return await self._find_elements(
+            query=self.get_query(),
+            filter_elm=filter_elm,
+            limit=limit,
+            page=page,
+        )
